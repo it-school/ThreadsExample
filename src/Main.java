eb25072be274ab1490f62a6f7eabf3877303373c
Viktor Speranskyy <vencedor@ukr.net>
2021-04-01 18:33:12 +0300

    upd2021

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,12 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.util.ArrayList;
+import classes.ChickenVoice;
+import classes.NetGet;
 
 public class Main {
-    public static void main(String[] args) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Document document = Jsoup.connect("https://google.com").timeout(30000).get();
-                    String text = document.text();
-                    System.out.println(text);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }).start();
 
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 1000; i++){
-            list.add(i);
-        }
-
-        for (Integer i : list) {
-            System.out.println(i);
-        }
-        System.out.println("Information written");
-    }
+   public static void main(String[] args) throws InterruptedException {
+      ChickenVoice.go();
+      NetGet.getResults();
+   }
 }
 
---
+++ src/classes/NetGet.java
@@ -0,0 +1,39 @@
+package classes;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+
+public class NetGet {
+   public static void getResults() throws InterruptedException {
+      final String[] text = {""};
+      ArrayList<String> list = new ArrayList<>();
+
+      for (int version = 7; version <= 16; version++) {
+         System.out.println("Thread for Java" + version);
+         int finalI = version;
+         new Thread(new Runnable() {
+            public void run() {
+               try {
+                  System.out.println("https://www.google.com/search?q=java" + finalI);
+                  Document document = Jsoup.connect("https://www.google.com/search?q=java" + finalI).timeout(10000).get();
+                  text[0] = document.text();
+
+               } catch (Exception ex) {
+                  ex.printStackTrace();
+               }
+            }
+         }).start();
+         Thread.sleep(1000);
+         list.add("" + finalI + ") " + text[0]);
+      }
+
+
+      for (String result : list) {
+         System.out.println(result + System.lineSeparator());
+      }
+   }
+
+
+}
